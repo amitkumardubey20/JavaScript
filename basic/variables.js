fcc97371bf52c05aa22279eb439ceecf9ac5cb43
Amit Kumar Dubey <97837093+amitkumardubey20@users.noreply.github.com>
2023-12-12 13:09:39 +0000

    variables info

---
+++ basic/variables.js
@@ -0,0 +1,22 @@
+const accountId=122;
+let accountEmail="amitkrdubeyyahoo.com";
+var accountPassword="1234"
+
+//accountId=123;
+
+// const types of variables can't be changed once assigned
+
+console.log(accountId); 
+accountCity="Mahoba"   
+
+let accountState
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
+// console.table ---> displays all the values in a table format
+// var is global scope
+// let is local scope
+// if a variable is not assigned then its output would be undefined.
+
+// You cannot re-declare a variable declared with let or const.
+// but with var we can re-declare the same variable.
