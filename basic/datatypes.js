86f8794d8a4ce6fde46f33204895a4da9215670b
Amit Kumar Dubey <97837093+amitkumardubey20@users.noreply.github.com>
2023-12-12 13:36:41 +0000

    commiting datatype

---
+++ basic/datatypes.js
@@ -0,0 +1,26 @@
+ "use strict" ; // trit all js code as the newwer version
+
+
+ // alert("welcome")  --> this will not work here because we are using node.js here
+ 
+ let name="AMit"
+ let age=23
+
+ // types of datatypes in js
+/*
+    number
+    bigint
+    boolean
+    symbol  ---> unique data type used in react
+    string
+    null --> it is a standalone value
+    undefined  --> when a variable is not initialized then it is of undefined type.
+     object 
+
+*/
+
+// to check the type of variables we have typeof keyword
+
+console.log(typeof age);
+console.log(typeof null)  // ---> type of null is Object 
+console.log(typeof undefined)   // ---> type of undefined is undefined
